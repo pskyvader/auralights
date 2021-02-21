79f54ed2b15194fc4ae35a8bb41f0d6b8588febf
Pablo Rain <pskyvader@hotmail.com>
2021-02-20 23:23:20 -0600

    --- Auto-Git Commit ---

--- test.py
+++ test.py
@@ -1,39 +1,36 @@
 import win32com.client
 import struct
 import time
+from colr import color
 
 auraSdk = win32com.client.Dispatch("aura.sdk.1")
 
 auraSdk.SwitchMode()
 
 devices = auraSdk.Enumerate(0)   # 0 means ALL
-for dev in devices:                      # Use enumeration
-    print(dev,dev.Lights.count,dev.name,dev.Type,dev.width,dev.height)
-    for i in range(dev.Lights.Count):    # Use index
-        current_light=dev.lights(i)
-        # current_light.color = 0x0000FF00
-        print(current_light.name,current_light.color,hex(current_light.color))
-        rgb = (current_light.blue,current_light.green,current_light.red)
-        print(bytes.hex(struct.pack('BBB',*rgb)))
-        # current_light.color=hex(0)
-        current_light.color = 0x00ffff00
-    dev.Apply()
+
+def get_light():
+    for dev in devices:                      # Use enumeration
+        # print(dev,dev.Lights.count,dev.name,dev.Type,dev.width,dev.height)
+        bar=dev.name
+        for i in range(dev.Lights.Count):    # Use index
+            current_light=dev.lights(i)
+            # current_light.color = 0x0000FF00
+            # print(current_light.name,current_light.color,hex(current_light.color))
+            rgb = (current_light.red,current_light.green,current_light.blue)
+            # print(rgb)
+            rgb=bytes.hex(struct.pack('BBB',*rgb))
+            bar+=color('|',fore=rgb, back=rgb)
+            # current_light.color=hex(0)
+            # current_light.color = 0x00ffff00
+        # dev.Apply()
+        print(bar)
 
 
-def print_format_table():
-    """
-    prints table of formatted text format options
-    """
-    for style in range(8):
-        for fg in range(30,38):
-            s1 = ''
-            for bg in range(40,48):
-                format = ';'.join([str(style), str(fg), str(bg)])
-                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
-            print(s1)
-        print('\n')
-
-print_format_table()
-
-time.sleep(5)
-print("asd")
+try:
+    while(True):
+        get_light()
+        print("ctrl-c")
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Stopped")
