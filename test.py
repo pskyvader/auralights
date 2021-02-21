21bfd9074fdf542cf8fe3a330ca014385147441d
Pablo Rain <pskyvader@hotmail.com>
2021-02-21 00:23:59 -0600

    --- Auto-Git Commit ---

--- test.py
+++ test.py
@@ -1,38 +1,49 @@
 import win32com.client
-import struct
+import random
 import time
 from colr import color
 import sys
 
 auraSdk = win32com.client.Dispatch("aura.sdk.1")
 
-# auraSdk.SwitchMode()
+auraSdk.SwitchMode()
 
 devices = auraSdk.Enumerate(0)   # 0 means ALL
 
 def get_light():
+    current_color=set_color()
     
-    current_light=devices(3).lights(0)
-    rgb = (current_light.blue,current_light.green,current_light.red)
-    print(rgb,hex(current_light.color))
+    # current_light=devices(0).lights(0)
+    # rgb = (current_light.red,current_light.green,current_light.blue)
+    # print(rgb,hex(current_light.color),current_light)
 
     for dev in devices:                      # Use enumeration
         # print(dev,dev.Lights.count,dev.name,dev.Type,dev.width,dev.height)
         # bar=dev.name
         bar=""
-        for i in range(dev.Lights.Count):    # Use index
+        for i in range(dev.Lights.count):    # Use index
             current_light=dev.lights(i)
-            # current_light.color = 0x0000FF00
-            # print(current_light.name,current_light.color,hex(current_light.color))
-            rgb = (current_light.blue,current_light.green,current_light.red)
-            # print(rgb)
-            # rgb=bytes.hex(struct.pack('BBB',*rgb))
-            # rgb=hex(current_light.color)
+            # print(rgb,hex(current_light.color),current_light.color)
+            current_light.color = current_color
+            rgb = (current_light.red,current_light.green,current_light.blue)
             bar+=color('|',fore=rgb, back=rgb)
-            # current_light.color=hex(0)
-            # current_light.color = 0x00ffff00
-        # dev.Apply()
+        dev.Apply()
         print(bar)
+
+
+def set_color():
+    r =int(random.random()*255)
+    b = int(random.random()*255)
+    g = int(random.random()*255)
+
+    color='0x00%02x%02x%02x' % (b, g, r)
+    return color
+    print(color)
+    color = int(color, 16)
+    color = hex(color)
+    print(color)
+    return color
+    
 
 
 try:
@@ -40,7 +51,7 @@ try:
         get_light()
         print("ctrl-c to exit")
         time.sleep(1)
-        for i in range(devices.count+2):
+        for i in range(devices.count+1):
             sys.stdout.write("\033[F") # Cursor up one line
 except KeyboardInterrupt:
     print("Stopped")
